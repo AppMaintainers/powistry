bb49f484fd143c967867346b2606cbf969d7314b
Botond Erdos <botond@appmaintainers.com>
2012-07-11 18:11:28 +0200

    Getting show and index to work (playing a bit with show to show complexity details)

--- app/models/complexity.rb
+++ app/models/complexity.rb
@@ -1,6 +1,7 @@
 class Complexity < ActiveRecord::Base
   attr_accessible :code, :planned_duration_max, :planned_duration_min, :points
-  def to_label
+  
+  def to_label #this the first thing SimpleForm looks for
     "#{code}"
   end
 
