62612e8ff4acde2c52083ebc18b177ae21dff78a
Botond Erdos <botond@appmaintainers.com>
2012-08-01 17:33:06 +0200

    mail trial 2

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Powistry::Application.initialize!
+
+# setting mail protocol for heroku (sendgrid)
+config.action_mailer.delivery_method = :smtp
+
+# in order to raise errors on mail delivery
+config.action_mailer.raise_delivery_errors = true
