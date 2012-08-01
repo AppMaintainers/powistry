398a074199ff535b2b4f03ec2ab5a205f5869aa0
Botond Erdos <botond@appmaintainers.com>
2012-08-01 16:47:39 +0200

    Email trial

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,9 @@
+class UserMailer < ActionMailer::Base
+  default from: "from@example.com"
+  
+  def welcome_email(user)
+    @user = user
+    #@url  = "http://example.com/login"
+    mail(:to => user.email, :subject => "Welcome to My Awesome Site")
+  end
+end
