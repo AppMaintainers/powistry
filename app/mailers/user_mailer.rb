62c1509bbe0caeab73249b6905043dccd0291421
Botond Erdos <botond@appmaintainers.com>
2012-08-02 02:00:05 +0200

    Next email test 7

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,6 +3,7 @@ class UserMailer < ActionMailer::Base
   
   def notification_email_on_unestimated_tasks(user)
     @user = user
+    attachments.inline['mail_notification.jpg'] = File.read('app/assets/images/mail_notification.jpg')
     mail(:to => user.email, :subject => "You have tasks to give estimations on") 
   end
 end
--- lib/tasks/send_email_notification.rake
+++ lib/tasks/send_email_notification.rake
@@ -1,3 +1,4 @@
+desc "This task send email about unfilled estimations daily at 8 am."
 task :send_email_notification => :environment do
   User.all.each do |user|
     if user.estimations.map{|e| e.complexity}.include?(nil)
