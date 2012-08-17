a2ddcf6b0670c365d0e3ed5b142466ee91e27bfd
Botond Erdos <botond@appmaintainers.com>
2012-08-17 17:09:47 +0200

    Queries done?

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -8,6 +8,22 @@ module TasksHelper
     end
   end
   
+  def get_average_min_hours(estimations)
+    if estimations.any? {|est| est.complexity.nil?}
+      return nil
+    else
+      mean = (estimations.map{|est| est.complexity.planned_duration_min}.sum.to_f/estimations.size*100).round.to_f/100
+    end
+  end
+  
+  def get_average_max_hours(estimations)
+    if estimations.any? {|est| est.complexity.nil?}
+      return nil
+    else
+      mean = (estimations.map{|est| est.complexity.planned_duration_max}.sum.to_f/estimations.size*100).round.to_f/100
+    end
+  end
+  
   def max_pts(task_list)
     task_list.collect{|t| t.user}.uniq.compact.map{|u| u.tasks.where(:id => task_list).map{|t| get_average_points(t.estimations)}.sum}.max
   end
@@ -18,6 +34,14 @@ module TasksHelper
   
   def user_sum_inv_hours(user)
     @tasks.where("user_id = ?", user.id).map{|t| t.invested_hours}.compact.sum
+  end
+  
+  def user_sum_min_hours(user)
+    @tasks.where("user_id = ?", user.id).map{|t| get_average_min_hours(t.estimations)}.sum
+  end
+  
+  def user_sum_max_hours(user)
+    @tasks.where("user_id = ?", user.id).map{|t| get_average_max_hours(t.estimations)}.sum
   end
 
 end
