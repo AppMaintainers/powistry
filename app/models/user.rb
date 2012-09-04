1c69bca2698e9b603629270db53ffab018def874
Botond Erdos <botond@appmaintainers.com>
2012-09-04 10:06:06 +0200

    Typo fixed, registration fixed, Tasks index page now show Project name instead of 'Listing tasks'

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,13 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  if Rails.env.production?
+    devise :database_authenticatable,
+           :recoverable, :rememberable, :trackable, :validatable
+  else
+    devise :database_authenticatable, :registerable,
+           :recoverable, :rememberable, :trackable, :validatable 
+  end
 
   # Setup accessible (or protected) attributes for your model
   
