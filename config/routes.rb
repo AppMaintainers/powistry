a91cc791560b85b0ff8248a1acf5ee95f03ce829
Attila Juhász <juhasz@atti.la>
2012-11-29 10:40:42 +0100

    Removed queries controller.

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,9 @@ Powistry::Application.routes.draw do
   
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
   
-  get "queries/index"
-  get "queries/search"
-  
   resources :estimations
   resources :complexities
-  resources :users do
-    resources :bookmarks
-  end
+  resources :users
   resources :projects do
     resources :tasks 
   end
